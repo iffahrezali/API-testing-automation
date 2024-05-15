a8186c58ec199ebd155f467dfa5f5f41e715084c
Iffah R <92578072+iffahrezali@users.noreply.github.com>
2024-05-15 08:01:49 +0800

    Add create, update and delete user functiona
    Add create, update and delete user functions using API response

--- api-automation.py
+++ api-automation.py
@@ -16,7 +16,7 @@ def test_api_request():
          "Data not matched! Expected: Holt, but found: " + str(record['last_name'])
 
 def test_api_response():
-  response = requests.post(url="",data=user_info.data)
+  response = requests.post(url="", data=user_info.data)
   user_info.data = response.json()
   assert (response.status_code == 201), "Status code is not 201. Rather found: " + str(response.status_code)
   assert user_info.data['name'] == "Simon", "User created with wrong name. \
@@ -24,3 +24,25 @@ def test_api_response():
   assert user_info.data['job'] == "QA", "User created with wrong job. \
      Expected: QA, but found: " + str(user_info.data['job'])
                                                     
+def test_create_user():
+  response = requests.post(url, json=user_info.new_data)
+  assert response.status_code == 201
+  user_info.data = response.json()
+  assert "id" in user_info.data
+  assert user_info.data['first_name'] == user_info.new_data['first_name']
+  assert user_info.data['last_name'] == user_info.mew_data['last_name']
+  
+def test_update_user():
+  user_id = 1
+  response = requests.put(f"{url}/{user_id}", json=updated_data)
+  assert response.status_code == 200
+  user_info.data = response.json()
+  assert user_info.data['first_name'] == user_info.updated_data['first_name']
+  assert user_info.data['last_name'] == user_info.updated_data['last_name']
+  
+def test_delete_user()
+  user_id = 1
+  response = requests.delete(f"{url}/{user_id}")
+  assert response.status_code == 204
+  get_response = requests.get(f"{url}/{user_id}")
+  assert get_responss.status_code == 404
